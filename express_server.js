29190ce4f705f44a2cb28f00078e10240ca76bb4
Konrad <konrad.kipczak@gmail.com>
2018-01-23 21:41:08 +0000

    created submission form and short id generator

--- express_server.js
+++ express_server.js
@@ -1,32 +1,51 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080; // default port 8080
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+let urlDatabase = {
+	"b2xVn2": "http://www.lighthouselabs.ca",
+	"9sm5xK": "http://www.google.com"
 };
 
 app.get("/", (req, res) => {
-  res.end("Hello!");
+	res.end("Hello!");
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+	let templateVars = { urls: urlDatabase };
+	res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+	res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id, fullURL: urlDatabase[req.params.id] };
-  res.render("urls_show", templateVars);
+	let templateVars = { shortURL: req.params.id, fullURL: urlDatabase[req.params.id] };
+	res.render("urls_show", templateVars);
 });
 
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
+app.post("/urls", (req, res) => {
+	console.log(req.body);  // debug statement to see POST parameters
+	res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+function generateRandomString() {
+	let chars = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+	let result = '';
+	for (let i = 0; i < 6; i++) {
+		result += chars[Math.floor(Math.random() * chars.length)];
+	}
+	console.log(result);
+}
+
+generateRandomString();
