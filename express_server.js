2dc815032601b0341186421c1fb28dd0cf98b7e0
Konrad <konrad.kipczak@gmail.com>
2018-01-24 16:24:58 +0000

    installed cookie parser

--- express_server.js
+++ express_server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080; // default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 app.set("view engine", "ejs");
 
 let urlDatabase = {
